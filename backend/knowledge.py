52b7677a082380a7a4898646b4c015f1835a165c
topone000659-blip <topone000659@gmail.com>
2026-07-10 23:53:44 +0630

    Update knowledge.py

--- backend/knowledge.py
+++ backend/knowledge.py
@@ -1,1 +1,17 @@
+class KnowledgeSystem:
+
+    def __init__(self):
+        self.knowledge = {}
+
+    def add(self, topic, information):
+        self.knowledge[topic] = information
+
+    def search(self, topic):
+        return self.knowledge.get(
+            topic,
+            "No information found."
+        )
+
+    def all_knowledge(self):
+        return self.knowledge
 
