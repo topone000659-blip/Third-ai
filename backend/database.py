99fa25e2f7e4d98d84283e771fd239190b12e88f
topone000659-blip <topone000659@gmail.com>
2026-07-11 00:15:23 +0630

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,14 @@
+class Database:
+
+    def __init__(self):
+        self.storage = []
+
+    def save(self, data):
+        self.storage.append(data)
+
+    def get_all(self):
+        return self.storage
+
+    def clear(self):
+        self.storage = []
 
