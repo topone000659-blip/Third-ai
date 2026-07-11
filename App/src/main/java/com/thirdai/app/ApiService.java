b6f2d8b1de87d2b494beb4a22e64648ef08bbc63
topone000659-blip <topone000659@gmail.com>
2026-07-11 16:00:51 +0630

    Update ApiService.java

--- App/src/main/java/com/thirdai/app/ApiService.java
+++ App/src/main/java/com/thirdai/app/ApiService.java
@@ -1,1 +1,15 @@
+package com.thirdai.app;
+
+public class ApiService {
+
+    public void sendMessage(String message) {
+
+        // Send message to backend
+        System.out.println(message);
+
+    }
+
+}
+
+
 
