f3f209e8c909fb07cec992cdb678922d84d70459
topone000659-blip <topone000659@gmail.com>
2026-07-11 16:28:26 +0630

    Refactor MainActivity and add message sending feature
    Refactor MainActivity to extend Activity and implement message sending functionality.

--- App/src/main/java/com/thirdai/app/MainActivity.java
+++ App/src/main/java/com/thirdai/app/MainActivity.java
@@ -1,10 +1,34 @@
 package com.thirdai.app;
 
-public class MainActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-    public void startApp() {
-        System.out.println("Third AI App Started");
+public class MainActivity extends Activity {
+
+    ApiService apiService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        apiService = new ApiService();
+
+        EditText input = findViewById(R.id.messageInput);
+        Button send = findViewById(R.id.sendButton);
+        TextView response = findViewById(R.id.responseText);
+
+        send.setOnClickListener(v -> {
+
+            String message = input.getText().toString();
+
+            apiService.sendMessage(message);
+
+            response.setText("Message sent: " + message);
+        });
     }
-
 }
-
