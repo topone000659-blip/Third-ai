e2efc4e94bd072be493cfde7d111701c520e85e2
topone000659-blip <topone000659@gmail.com>
2026-07-11 20:42:34 +0630

    Update ApiService.java

--- App/src/main/java/com/thirdai/app/ApiService.java
+++ App/src/main/java/com/thirdai/app/ApiService.java
@@ -1,15 +1,57 @@
 package com.thirdai.app;
 
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ApiService {
+
+    private static final String API_URL = "http://YOUR_SERVER_IP:8000/chat";
 
     public void sendMessage(String message) {
 
-        // Send message to backend
-        System.out.println(message);
+        new Thread(() -> {
 
+            try {
+
+                URL url = new URL(API_URL);
+
+                HttpURLConnection connection =
+                        (HttpURLConnection) url.openConnection();
+
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty(
+                        "Content-Type",
+                        "application/json"
+                );
+
+                connection.setDoOutput(true);
+
+                String json =
+                        "{\"message\":\"" + message + "\"}";
+
+                OutputStream os =
+                        connection.getOutputStream();
+
+                os.write(json.getBytes());
+                os.flush();
+                os.close();
+
+                int responseCode =
+                        connection.getResponseCode();
+
+                System.out.println(
+                        "API Response: " + responseCode
+                );
+
+                connection.disconnect();
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+        }).start();
     }
-
 }
-
-
-
