85dcd93925ea77b1a0921056cdb9ceb41cabe2b8
topone000659-blip <topone000659@gmail.com>
2026-07-12 21:01:42 +0630

    Update SettingsActivity.java

--- App/src/main/java/com/thirdai/app/SettingsActivity.java
+++ App/src/main/java/com/thirdai/app/SettingsActivity.java
@@ -1,1 +1,14 @@
+package com.thirdai.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
 
