7a8b60bbbc54ac9741c62898ab5f7af0cf4d9b1a
topone000659-blip <topone000659@gmail.com>
2026-07-13 00:07:33 +0630

    Update LanguageActivity.java

--- App/src/main/java/com/thirdai/app/LanguageActivity.java
+++ App/src/main/java/com/thirdai/app/LanguageActivity.java
@@ -1,1 +1,16 @@
+package com.thirdai.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LanguageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_language);
+    }
+}
+
 
