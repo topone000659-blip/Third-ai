e3dca67cac1fd438822a24afb2f1cf68845600e2
topone000659-blip <topone000659@gmail.com>
2026-07-12 20:48:17 +0630

    Update Message.java

--- App/src/main/java/com/thirdai/app/Message.java
+++ App/src/main/java/com/thirdai/app/Message.java
@@ -1,1 +1,21 @@
+package com.thirdai.app;
+
+public class Message {
+
+    private String text;
+    private boolean isUser;
+
+    public Message(String text, boolean isUser) {
+        this.text = text;
+        this.isUser = isUser;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isUser() {
+        return isUser;
+    }
+}
 
