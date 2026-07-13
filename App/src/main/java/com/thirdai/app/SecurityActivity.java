d571b4344565fbc29c11c50b47fb4deee9921c62
topone000659-blip <topone000659@gmail.com>
2026-07-13 14:22:29 +0630

    Update SecurityActivity.java

--- App/src/main/java/com/thirdai/app/SecurityActivity.java
+++ App/src/main/java/com/thirdai/app/SecurityActivity.java
@@ -1,1 +1,14 @@
+package com.thirdai.app;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecurityActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_security);
+    }
+}
