11d963c2a06080d4d1b65bfcd6e6883e16586162
topone000659-blip <topone000659@gmail.com>
2026-07-13 11:50:24 +0630

    Update ClearChatsActivity.java

--- App/src/main/java/com/thirdai/app/ClearChatsActivity.java
+++ App/src/main/java/com/thirdai/app/ClearChatsActivity.java
@@ -1,1 +1,22 @@
+package com.thirdai.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ClearChatsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clear_chats);
+
+        Button clearButton = findViewById(R.id.clearChatsButton);
+
+        clearButton.setOnClickListener(v ->
+                Toast.makeText(this, "Chat history cleared", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
 
