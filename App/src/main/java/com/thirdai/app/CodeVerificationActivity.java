4096e896d601f86c0fb35b2c84f627359cb32bd2
topone000659-blip <topone000659@gmail.com>
2026-07-15 14:35:43 +0630

    Update CodeVerificationActivity.java

--- App/src/main/java/com/thirdai/app/CodeVerificationActivity.java
+++ App/src/main/java/com/thirdai/app/CodeVerificationActivity.java
@@ -1,21 +1,21 @@
 package com.thirdai.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx app. Activity.
-
 public class CodeVerificationActivity extends Activity {
 
-    private EditText codeEditText;
-    private Button verifyButton;
+    EditText codeEditText;
+    Button verifyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_code_verification);
 
         codeEditText = findViewById(R.id.codeEditText);
@@ -27,7 +27,11 @@ public class CodeVerificationActivity extends Activity {
 
             if (code.equals("123456")) {
 
-                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+                Toast.makeText(
+                        CodeVerificationActivity.this,
+                        "Verification Successful",
+                        Toast.LENGTH_SHORT
+                ).show();
 
                 Intent intent = new Intent(
                         CodeVerificationActivity.this,
@@ -39,13 +43,14 @@ public class CodeVerificationActivity extends Activity {
 
             } else {
 
-                Toast.makeText(this,
-                        "Invalid Verification Code",
-                        Toast.LENGTH_SHORT).show();
+                Toast.makeText(
+                        CodeVerificationActivity.this,
+                        "Wrong Code",
+                        Toast.LENGTH_SHORT
+                ).show();
 
             }
 
         });
-
     }
 }
