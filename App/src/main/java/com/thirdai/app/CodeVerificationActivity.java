0b952c11128470565fa16960fea0dca937a32393
topone000659-blip <topone000659@gmail.com>
2026-07-15 12:20:51 +0630

    Add CodeVerificationActivity class implementation

--- App/src/main/java/com/thirdai/app/CodeVerificationActivity.java
+++ App/src/main/java/com/thirdai/app/CodeVerificationActivity.java
@@ -1,1 +1,14 @@
+package com.thirdai.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CodeVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_verification);
+    }
+}
 
