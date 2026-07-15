8d569dd3d42aa5a646df7fb11380beee65ee3a98
topone000659-blip <topone000659@gmail.com>
2026-07-15 12:18:01 +0630

    Update DeleteAccountActivity.java

--- App/src/main/java/com/thirdai/app/DeleteAccountActivity.java
+++ App/src/main/java/com/thirdai/app/DeleteAccountActivity.java
@@ -1,1 +1,14 @@
+package com.thirdai.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DeleteAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_account);
+    }
+}
 
