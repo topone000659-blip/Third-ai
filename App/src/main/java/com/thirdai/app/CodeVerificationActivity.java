5fab015844cad8664af1ce86335f3b370290c2bf
topone000659-blip <topone000659@gmail.com>
2026-07-15 13:38:13 +0630

    Update CodeVerificationActivity.java

--- App/src/main/java/com/thirdai/app/CodeVerificationActivity.java
+++ App/src/main/java/com/thirdai/app/CodeVerificationActivity.java
@@ -6,7 +6,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidxActivity;
+import androidxActivity
 
 public class CodeVerificationActivity extends Activity {
 
