fae10ec888357410a2c9cb009dfd6274d1dfce0c
topone000659-blip <topone000659@gmail.com>
2026-07-15 13:25:29 +0630

    Change superclass from AppCompatActivity to Activity

--- App/src/main/java/com/thirdai/app/CodeVerificationActivity.java
+++ App/src/main/java/com/thirdai/app/CodeVerificationActivity.java
@@ -6,7 +6,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.Activity;
 
 public class CodeVerificationActivity extends Activity {
 
